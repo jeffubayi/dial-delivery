033ae2d58d59daade7cba1fa601c7d06f0e5b4a7
jeffubayi <jeffubayi@gmail.com>
2022-02-18 17:14:14 +0300

    update - format components folder using prettier

---
+++ src/Components/Finish/Finish.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GlobalContext } from '../../GlobalContext';
+import styles from './Finish.module.css';
+import CartItem from '../Home/Cart/CartItem';
+
+const Finish = () => {
+  const { cart, total, typeBuy } = React.useContext(GlobalContext);
+
+  return (
+    <div className={styles.finishContainer}>
+      <div className={styles.orderFinish}>
+        <ion-icon name="checkmark-circle"></ion-icon>
+        <h2>Order completed</h2>
+      </div>
+      <div className={styles.orderContainer}>
+        <div className={styles.orderItems}>
+          {cart.map((item) => <CartItem key={item.id} product={item} isFinish={true} />)}
+        </div>
+        <div className={styles.total}>
+          <h4>Total</h4>
+          <h4 className={styles.price}>Ksh {total},00</h4>
+        </div>
+      </div>
+      {typeBuy === "delivery" ? (
+        <p className={styles.orderStore}>Your order will be delivered within 60 minutes.</p>
+      ) : (
+        <p className={styles.orderStore}>
+        In 25 minutes your order will be ready to be picked up in our store.</p>
+      )}
+    </div>
+  )
+}
+
+export default Finish;
