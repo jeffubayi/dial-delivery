ebb41e72522172c53c59edcc34e80ed357a6fa4b
jeffubayi <jeffubayi@gmail.com>
2022-02-21 15:46:31 +0300

    payments - add mpesa payment redirect

--- src/Components/Checkout/Form/Fragments/RadioPayment.js
+++ src/Components/Checkout/Form/Fragments/RadioPayment.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { GlobalContext } from '../../../../GlobalContext';
-import styles from './RadioPayment.module.css';
+import React from "react";
+import { GlobalContext } from "../../../../GlobalContext";
+import styles from "./RadioPayment.module.css";
 
 const RadioPayment = () => {
   const { typePayment, setTypePayment } = React.useContext(GlobalContext);
@@ -8,24 +8,46 @@ const RadioPayment = () => {
   return (
     <div className={styles.radioPayment}>
       <label className={styles.label}>
-        <input name="card" type="radio" value="card" className={styles.radio} checked={typePayment === "card"} onChange={({ target }) => setTypePayment(target.value)} />
+        <input
+          name="card"
+          type="radio"
+          value="card"
+          className={styles.radio}
+          checked={typePayment === "card"}
+          onChange={({ target }) => setTypePayment(target.value)}
+        />
         Mpesa
       </label>
       <label className={styles.label}>
-        <input name="money" type="radio" value="money" className={styles.radio} checked={typePayment === "money"} onChange={({ target }) => setTypePayment(target.value)} />
+        <input
+          name="money"
+          type="radio"
+          value="money"
+          className={styles.radio}
+          checked={typePayment === "money"}
+          onChange={({ target }) => setTypePayment(target.value)}
+        />
         Cash on Delivery
-      </label> 
-      {typePayment === 'card'  ? (
+      </label>
+      {typePayment === "card" ? (
         <>
-         <div className={styles.bgImage}>
-        <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2-PDP42uIR-b9fMNTyDywET8-gVryZnPAhO3pK8FC7o8KHeKFVYxO-hc2VIBN06NROSk&usqp=CAU" alt="" />
-      </div>
+          <div>
+            <img
+              className={styles.bgImage}
+              src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2-PDP42uIR-b9fMNTyDywET8-gVryZnPAhO3pK8FC7o8KHeKFVYxO-hc2VIBN06NROSk&usqp=CAU"
+              alt=""
+            />
+
+            <label className={styles.text}>
+              You'll receive a prompt to complete the payment
+            </label>
+          </div>
         </>
       ) : (
-        ''
+        ""
       )}
     </div>
-  )
-}
+  );
+};
 
 export default RadioPayment;
