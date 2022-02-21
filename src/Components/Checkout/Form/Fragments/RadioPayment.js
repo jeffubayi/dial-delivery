1d0ddb532c3c0b3a9901afe3f704ca3ad0c6fa17
jeffubayi <jeffubayi@gmail.com>
2022-02-21 15:45:52 +0300

    add mpesa prompt

--- src/Components/Checkout/Form/Fragments/RadioPayment.js
+++ src/Components/Checkout/Form/Fragments/RadioPayment.js
@@ -14,7 +14,16 @@ const RadioPayment = () => {
       <label className={styles.label}>
         <input name="money" type="radio" value="money" className={styles.radio} checked={typePayment === "money"} onChange={({ target }) => setTypePayment(target.value)} />
         Cash on Delivery
-      </label>
+      </label> 
+      {typePayment === 'card'  ? (
+        <>
+         <div className={styles.bgImage}>
+        <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2-PDP42uIR-b9fMNTyDywET8-gVryZnPAhO3pK8FC7o8KHeKFVYxO-hc2VIBN06NROSk&usqp=CAU" alt="" />
+      </div>
+        </>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
